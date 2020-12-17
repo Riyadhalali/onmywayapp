0c3841c07453356aea5ce6a54727c114b2527e21
Riyad <eng.riyadalali93@gmail.com>
2020-12-17 09:47:54 +0200

    Register Activity: 1- Added Background to register activity.

--- lib/ui/register.dart
+++ lib/ui/register.dart
@@ -1,4 +1,6 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class Register extends StatefulWidget {
   static const id = 'Register';
@@ -9,6 +11,69 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: columnElements(),
+    );
+  } // end builder
+
+  //-------------------------------Column --------------------------------------
+  Widget columnElements() {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            imageBackground(),
+            registerContainer(),
+          ],
+        ),
+      ),
+    );
   }
-}
+
+//---------------------------Column Elements------------------------------------
+  Widget imageBackground() {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.3,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/ui/register/register.png'),
+              fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+
+  //-> Container for having elements
+  Widget registerContainer() {
+    return Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50.0),
+            color: Color(0xFFf2f2f2),
+          ),
+        ),
+        Column(
+          children: [
+            SizedBox(
+              height: 5.0.h,
+            ),
+            Text(
+              "register".tr().toString(),
+              style: TextStyle(
+                  fontSize: 20.0.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+//------------------------------------------------------------------------------
+} //end class

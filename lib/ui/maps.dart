02792ec92849d6a8891f2495b1ebc8be24d65d50
Riyad <eng.riyadalali93@gmail.com>
2020-12-28 11:13:27 +0200

    Worked With geolocator to get location of the device. but i used version 3.0.1 of geolocator because the last release have problems with build

--- lib/ui/maps.dart
+++ lib/ui/maps.dart
@@ -1,25 +1,45 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sizer/sizer.dart';
 
 class Maps extends StatefulWidget {
   static const id = 'maps_page';
+
   @override
   _MapsState createState() => _MapsState();
 }
 
 class _MapsState extends State<Maps> {
+  static double latLocation;
+  static double langLocation;
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 50.0,
+    target: LatLng(35.1365364, 36.7870555),
+    zoom: 30.0,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      target: LatLng(37.43296265331129, -122.08832357078792), zoom: 20.0);
+  static final CameraPosition _kLake =
+      CameraPosition(target: LatLng(latLocation, langLocation), zoom: 15.0);
+
+  void getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    latLocation = position.latitude;
+    langLocation = position.longitude;
+    print(position);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    // _determinePosition();
+  }
 
   @override
   Widget build(BuildContext context) {

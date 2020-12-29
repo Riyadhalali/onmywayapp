d193c18f7707168bd22bc20488da958e36afdc9d
Riyad <eng.riyadalali93@gmail.com>
2020-12-29 10:47:30 +0200

    Maps UI - we can not use the future builder with maps loading and getting location. - we used instead of that in body of scaffold a state to check if null then load a circle porgress indicator, and when done loading the getting location geolocator the maps will pop out. - getting location and display it on map. - used marker on screen. - changeview of maps also has been used.

--- lib/ui/maps.dart
+++ lib/ui/maps.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,59 +14,134 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
-  static double latLocation;
-  static double langLocation;
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(35.1365364, 36.7870555),
-    zoom: 30.0,
-  );
-
-  static final CameraPosition _kLake =
-      CameraPosition(target: LatLng(latLocation, langLocation), zoom: 15.0);
-
+  Completer<GoogleMapController> controller1;
+  static LatLng _initialPosition;
+  final Set<Marker> _markers = {};
+  static LatLng _lastMapPositon = _initialPosition;
+  MapType _currentMapType = MapType.normal; // define the map type
+  //-----------------------Get User Location------------------------------------
   void getLocation() async {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-    latLocation = position.latitude;
-    langLocation = position.longitude;
-    print(position);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      print(_initialPosition);
+    });
+  }
+
+//-----------------------on Map Created-----------------------------------------
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      controller1.complete(controller);
+    });
+  }
+
+  //----------------Change Map type using Button--------------------------------
+  _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  //--------------------------on Camera Move -----------------------------------
+  _onCameraMove(CameraPosition position) {
+    _lastMapPositon = position.target;
+  }
+
+  //-------------------------------Add marker Button----------------------------
+  _onAddMarkerButtonPressed() {
+    setState(() {
+      print('marker is pressed');
+      _markers.add(
+        Marker(
+          markerId: MarkerId(_lastMapPositon.toString()),
+          position: _lastMapPositon,
+          infoWindow: InfoWindow(title: "Pizza"),
+          onTap: () {},
+          icon: BitmapDescriptor.defaultMarker,
+        ),
+      );
+    });
+  }
+
+//----------------------------MapButton-----------------------------------------
+  Widget mapButton(Function function, Icon icon, Color color) {
+    return RawMaterialButton(
+      onPressed: function,
+      child: icon,
+      shape: new CircleBorder(),
+      elevation: 2.0,
+      fillColor: color,
+      padding: const EdgeInsets.all(7.0),
+    );
+  }
+  //-------------------------------Camera Widget--------------------------------
+
+  Widget CameraUI() {
+    return Container(
+      height: 70.0.h,
+      child: Stack(
+        children: [
+          GoogleMap(
+            markers: _markers,
+            mapType: _currentMapType,
+            initialCameraPosition: CameraPosition(
+              target: _initialPosition,
+              zoom: 14.4746,
+            ),
+            onCameraMove: _onCameraMove,
+            onMapCreated: _onMapCreated,
+            myLocationEnabled: true, // very important to get the location
+            compassEnabled: true,
+            myLocationButtonEnabled: true,
+            zoomGesturesEnabled: true,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+              child: Column(
+                children: [
+                  mapButton(_onAddMarkerButtonPressed(),
+                      Icon(Icons.add_location), Colors.blue),
+                  mapButton(
+                      _onMapTypeButtonPressed(),
+                      Icon(
+                        IconData(0xf473,
+                            fontFamily: CupertinoIcons.iconFont,
+                            fontPackage: CupertinoIcons.iconFontPackage),
+                      ),
+                      Colors.green),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    print("maps started...");
     getLocation();
-    // _determinePosition();
   }
 
+//-----------------------------------Build Widget-------------------------------
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Maps'),
-      ),
-      body: MapsWidget(),
-    );
-  } // end build
-
-//------------------------------Widget Tree-------------------------------------
-  Widget MapsWidget() {
-    return Container(
-      height: 50.0.h,
-      child: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+      body: _initialPosition == null
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : CameraUI(),
     );
   }
-//------------------------------------------------------------------------------
-
-} // end class
-
-//TODO: use geoloactor
+}
